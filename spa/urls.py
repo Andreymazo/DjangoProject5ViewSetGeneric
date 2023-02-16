fe359c51addb3d3db4fa5648742e0c32640eaa93
andrey_mazo <andreymazo@mail.ru>
2023-02-16 13:51:40 +0300

    prilozh inicializ

--- spa/urls.py
+++ spa/urls.py
@@ -5,12 +5,12 @@ from django.urls import path
 from config import settings
 from spa.apps import SpaConfig
 from spa.views import SigninView, SignupView, CourseViewset
-
+app_name = SpaConfig.name##Ne pokazal nam obyavlenia prilozhenia
 router = DefaultRouter()
 router.register(r'home', CourseViewset, basename='home')
 
 urlpatterns = [] + router.urls
-# app_name = SpaConfig.name
+#
 #     [
 #     path('', SigninView.as_view(template_name='spa/login.html'), name='login'),
 #     path('register', SignupView.as_view(template_name='spa/register.html'), name='register'),
