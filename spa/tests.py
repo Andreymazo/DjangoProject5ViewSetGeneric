8af11c39e6db52efe02e159a884cab24917936f6
andrey_mazo <andreymazo@mail.ru>
2023-02-28 15:29:38 +0300

    тест на вебинаре 28.фев23г с Маратом дорабатывали, пока валится, но уже лучше, чем было)

--- spa/tests.py
+++ spa/tests.py
@@ -1,24 +1,37 @@
+# from django.contrib.auth.models import User
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import django
+django.setup()
 from rest_framework import status
-from rest_framework.authtoken.admin import User
+
 from rest_framework.test import APITestCase
 
+from config import settings
+
+
+from spa.models import CustomUser
 class UserSubscriptionTestCase(APITestCase):
     def setUp(self)->None:
-        self.user = User (
-            username='test@test.ru',
+
+        self.user = CustomUser(
+            email='test@12.ru',
         )
-        self.user_set_password('qwert123asd')
+        self.user.set_password('qwert123asd')
         self.user.save()
         response = self.client.post(
             '/api/token/',
-            {'username': 'test@test.ru', 'password': 'qwert123asd'}
+            {'email': 'test@12.ru', 'password': 'qwert123asd'},
+            content_type="application/json"
         )
+        print(response.status_code)
         self.access_token = response.json().get('access')
+
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-    def UserSubscription_create(self):
+    def test_create(self):
         response = self.client.post(
-            'home/UserSubscription/',
-        {"user": 1}
+            "/home/UserSubscription/",
+        {"user": 6}
         )
         self.assertEqual(
             response.status_code,
